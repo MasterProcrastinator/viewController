d39effdb67569bc007cdaab7c9d46a98ef0b1633
Alvin <thisiseasytoremember1224@gmail.com>
2023-10-05 09:07:08 -0500

    notes

--- viewController/ViewController.swift
+++ viewController/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textFieldOutlet: UITextField!
+    
+    var pony = ""
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = UIColor.yellow
+ 
         // Do any additional setup after loading the view.
     }
 
-
+    override func viewWillAppear(_ animated: Bool) {
+        print(pony)
+    }
+    
+    
+    
+    
+    @IBAction func manualAction(_ sender: UIButton) {
+        pony = textFieldOutlet.text!
+        view.backgroundColor = UIColor.brown
+        performSegue(withIdentifier: "toRed", sender: self)
+    }
+    //called before segue happens
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //NVC (next view controller) giving access to the next view controller
+        let nvc = segue.destination as! ViewController2
+        nvc.name = pony
+        nvc.delegate = self
+            
+        
+    }
+    
 }
 
---
+++ viewController/ViewController2.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController2.swift
+//  viewController
+//
+//  Created by ALVIN CHEN on 9/28/23.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+    
+    var name: String!
+    
+    //creating a var to get access to first view controller
+    var delegate: ViewController!
+    
+    @IBOutlet weak var textFieldOutlet: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+print(name)
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func buttonAction(_ sender: UIButton) {
+        
+        view.backgroundColor = UIColor.yellow
+    }
+    
+    @IBAction func buttonAction2(_ sender: UIButton) {
+        
+        performSegue(withIdentifier: "segue2", sender: self)
+    }
+    
+    @IBAction func changesName(_ sender: UIButton) {
+        var name2 = textFieldOutlet.text!
+        delegate.pony = name2
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
